64c0e8153e931e3211694ed48886bfce76116938
Administrator <Administrator@DESKTOP-1B1BE1O>
2023-04-30 20:10:44 +0100

    I have added :
    the FilmRepository and the FilmController class which includes a
    commented out save method that allows adding an image for the film.
    However, I decided to leave it because the Film class does not have a
    photo attribute. What do you think?

--- spring-adminlte2/src/main/java/com/hendisantika/adminlte/repository/FilmRepository.java
+++ spring-adminlte2/src/main/java/com/hendisantika/adminlte/repository/FilmRepository.java
@@ -1,5 +1,5 @@
 package com.hendisantika.adminlte.repository;
-
+//FilmRepository
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.hendisantika.adminlte.model.Film;
